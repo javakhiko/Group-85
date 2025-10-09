c0d2ff05c5569c63ec40edba5f5a61e8b938f290
javakhiko <javaxishvili.andri@gmail.com>
2025-10-09 14:41:39 +0400

    commit

---
+++ day03/homework/hw.py
@@ -0,0 +1,21 @@
+name=input("Hello:")
+
+print("Hello", name )
+
+#hello darling
+
+age=input("your age:")    
+print("you are", age, "years old")
+
+#are u under 18?? ;-;
+
+food=input("Your Favorite Food:")
+print("Your favorite food is", food )  
+
+#woow i love that food !
+text=input("please enter random text: ")
+print("######################")
+print("#####", text, "#########")
+print("######################")
+
+#/hw FINISH
